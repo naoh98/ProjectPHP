2954f8a4584362675ac86b9a60c39382eca3dc3f
AntonDev07 <anhquangtong2000@gmail.com>
2020-08-10 17:26:46 +0700

    anhtong cmmit

--- electronic_store/app/Http/Controllers/Frontend/IndexController.php
+++ electronic_store/app/Http/Controllers/Frontend/IndexController.php
@@ -8,6 +8,12 @@ use Illuminate\Http\Request;
 class IndexController extends Controller
 {
     //
+//    public function __construct()
+//    {
+//        $this->middleware('auth');
+//    }
+
+
     public function index(){
         return view('frontend.contents.homepage');
     }
